bf8bec2b5333bdad3a4f06eba51a2443e77fb818
M-Basi <m.mpasinas@gmail.com>
2025-01-02 00:05:27 +0200

    locale el

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,17 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptorService } from './shared/interfaces/auth.interceptor';
+import { registerLocaleData } from '@angular/common';
+import { LOCALE_ID } from '@angular/core';
+import localeEl from '@angular/common/locales/el';
+
+// Καταχώρηση της ελληνικής τοπικής ρύθμισης
+registerLocaleData(localeEl);
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
   provideRouter(routes), 
+  { provide: LOCALE_ID, useValue: 'el' } ,
   provideAnimationsAsync(),
   provideHttpClient(
     withInterceptorsFromDi()
